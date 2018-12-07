5a8b040633e989e8a9664ab4041834468453b39a
Erdem <ege.marasli@ug.bilkent.edu.tr>
2018-12-07 20:15:12 +0300

    Test Deleted

--- src/TheWall/Knight.java
+++ src/TheWall/Knight.java
@@ -9,8 +9,5 @@ public class Knight extends GameUnits{
     public Knight(int x, int y,int radius,boolean isEnemy){
         super(x,y,radius,isEnemy);
     }
-    public void testFoo(){
-        System.out.println("test");
-    }
 
 }
