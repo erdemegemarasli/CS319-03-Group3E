a95e56e60a63b1d7d8548fe3d5a4fb34a7437f70
aeyc <abegumtascioglu@gmail.com>
2018-12-12 01:12:41 +0300

    Knight renamed

--- src/TheWall/Knight.java
+++ src/TheWall/Knight.java
@@ -5,8 +5,8 @@ import java.awt.*;
 Creator: Ayca Begum Tascioglu
  */
 
-public class Knight extends GameUnits{
-    public Knight(int x, int y,int radius,boolean isEnemy){
+public class GameUnit extends GameUnits{
+    public GameUnit(int x, int y,int radius,boolean isEnemy){
         super(x,y,radius,isEnemy);
     }
 
