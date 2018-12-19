c1bf1e71b02176d205b45bc82fe0dca5ae065772
aeyc <abegumtascioglu@gmail.com>
2018-12-19 21:21:23 +0300

    knight class renamed

--- src/TheWall/GameUnit.java
+++ src/TheWall/GameUnit.java
@@ -9,7 +9,7 @@ Creator: Ayca Begum Tascioglu
 public class GameUnit extends GameUnits{
     public GameUnit(int x, int y,int radius,boolean isEnemy){
         super(x,y,radius,isEnemy);
-        //test
+        //new class knight renamed
     }
 
 }
