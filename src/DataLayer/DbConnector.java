e0e94286365000096f7ac036bfc6635f46b23643
ensarkaya <ensarben@gmail.com>
2018-12-19 12:55:07 +0300

    Update DbConnector.java

--- src/DataLayer/DbConnector.java
+++ src/DataLayer/DbConnector.java
@@ -1,10 +1,16 @@
-
 package DataLayer;
 
+import java.sql.*;
 
 /*
 Creator: Ensar Kaya
  */
 public class DbConnector {
+
+	protected static Connection con;
+	//public static int id=1;
+	protected static final String USERNAME = "root";
+	protected static final String PASSWORD = "Ensarben1..4.j83796";
+	protected static final String CONN_STRING = "jdbc:mysql://localhost/logininfo?useUnicode=true&useLegacyDatetimeCode=false&serverTimezone=Turkey";
 
 }
