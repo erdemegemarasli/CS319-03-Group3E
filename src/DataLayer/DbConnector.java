3e1f878a070dfb87ec6582a479fb0b0d6db6b4f8
ensarkaya <ensarben@gmail.com>
2018-12-19 12:57:22 +0300

    Update DbConnector.java

--- src/DataLayer/DbConnector.java
+++ src/DataLayer/DbConnector.java
@@ -5,9 +5,8 @@ import java.sql.*;
 public class DbConnector {
 	//ATTRIBUTES
 	protected static Connection con;
-	//public static int id=1;
 	protected static final String USERNAME = "root";
-	protected static final String PASSWORD = "Ensarben1..4.j83796";
+	protected static final String PASSWORD = "";
 	protected static final String CONN_STRING = "jdbc:mysql://localhost/logininfo?useUnicode=true&useLegacyDatetimeCode=false&serverTimezone=Turkey";
 
 	/** 
@@ -27,5 +26,33 @@ public class DbConnector {
 		return con;
 	}
 	
+	/** 
+	 *This method search username, password parameters in mytable
+	 *return -1 if it's not founded 
+	 *return an positive integer which is the index number 
+	 * @param     
+	 * @return	It returns int ret which is an positive integer when user exist in the table or -1 if its not exist		
+	 */
+	public static int verifyUser(String name, String password){
+		int ret=-1;
+		String n,p;
+		try{
+			con = getConnection();
+			Statement stat = con.createStatement();
+			String sql = "SELECT username, password,ID FROM mytable";
+			ResultSet rs=stat.executeQuery(sql);
+			while(rs.next())
+			{
+				n=rs.getString("username");
+				p=rs.getString("password");
+				if(n.equals(name)&&p.equals(password))
+					ret= rs.getInt("ID");//index of founded row
+			}
+			rs.close();
+		}
+		catch(SQLException e){
+			e.printStackTrace();}		
+		return ret;}
+
 	
 }
