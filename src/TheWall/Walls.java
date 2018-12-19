aabedee2d772456a525196e431fdca37f38e0dee
aeyc <abegumtascioglu@gmail.com>
2018-12-19 23:46:32 +0300

    methods of fixeds

--- src/TheWall/Walls.java
+++ src/TheWall/Walls.java
@@ -21,67 +21,68 @@ public class Walls{
     public Walls() {
         //create first wall
         WallEdge wallEdges0[] = new WallEdge[4];
-        wallEdges0[0] = new WallEdge(x, y, radius,true);
-        wallEdges0[1] = new WallEdge(x+radius+ horizontalWidth , y, radius,false);
-        wallEdges0[2] = new WallEdge(x, y+radius+verticalHeight, radius,false);
-        wallEdges0[3] = new WallEdge(x+ horizontalWidth+radius , y+radius+verticalHeight, radius,false);
+        wallEdges0[0] = new WallEdge(x, y, radius,true, x ,y);
+        wallEdges0[1] = new WallEdge(x+radius+ horizontalWidth , y, radius,false,
+                                x+radius+ horizontalWidth,y);
+        wallEdges0[2] = new WallEdge(x, y+radius+verticalHeight, radius,false, x, y+radius+verticalHeight);
+        wallEdges0[3] = new WallEdge(x+ horizontalWidth+radius , y+radius+verticalHeight, radius,false,x+ horizontalWidth+radius , y+radius+verticalHeight);
 
         WallLine wallLines0[] = new WallLine[3];
-        wallLines0[0] = new WallLine(x+radius, y , horizontalWidth , horizontalHeight);
-        wallLines0[1] = new WallLine(x , y+radius, verticalWidth, verticalHeight);
-        wallLines0[2] = new WallLine(x+ verticalHeight+radius, y+radius, verticalWidth, verticalHeight);
+        wallLines0[0] = new WallLine(x+radius, y , horizontalWidth , horizontalHeight, x+radius, y);
+        wallLines0[1] = new WallLine(x , y+radius, verticalWidth, verticalHeight,x , y+radius);
+        wallLines0[2] = new WallLine(x+ verticalHeight+radius, y+radius, verticalWidth, verticalHeight,x+ verticalHeight+radius, y+radius);
 
         //create second wall
         y += 120;
         WallEdge wallEdges1[] = new WallEdge[7];
-        wallEdges1[0] = new WallEdge(x, y, radius,false);
-        wallEdges1[1] = new WallEdge(x + radius+horizontalWidth, y, radius,false);
-        wallEdges1[2] = new WallEdge(x + radius+horizontalWidth, y + verticalHeight+radius, radius,false);
-        wallEdges1[3] = new WallEdge(x + 2*radius + 2*horizontalWidth, y , radius,false);
-        wallEdges1[4] = new WallEdge(x + 2*horizontalWidth+2*radius, y +verticalHeight+radius, radius,false);
-        wallEdges1[5] = new WallEdge(x, y+radius+verticalHeight, radius,false);
-        wallEdges1[6] = new WallEdge(x + 3*horizontalWidth+3*radius, y, radius,false);
+        wallEdges1[0] = new WallEdge(x, y, radius,false,x, y);
+        wallEdges1[1] = new WallEdge(x + radius+horizontalWidth, y, radius,false,x + radius+horizontalWidth, y);
+        wallEdges1[2] = new WallEdge(x + radius+horizontalWidth, y + verticalHeight+radius, radius,false,x + radius+horizontalWidth, y + verticalHeight+radius);
+        wallEdges1[3] = new WallEdge(x + 2*radius + 2*horizontalWidth, y , radius,false,x + 2*radius + 2*horizontalWidth, y);
+        wallEdges1[4] = new WallEdge(x + 2*horizontalWidth+2*radius, y +verticalHeight+radius, radius,false,x + 2*horizontalWidth+2*radius, y +verticalHeight+radius);
+        wallEdges1[5] = new WallEdge(x, y+radius+verticalHeight, radius,false,x, y+radius+verticalHeight);
+        wallEdges1[6] = new WallEdge(x + 3*horizontalWidth+3*radius, y, radius,false,x + 3*horizontalWidth+3*radius, y);
 
 
         WallLine wallLines1[] = new WallLine[6];
-        wallLines1[0] = new WallLine(x, y, verticalWidth, verticalHeight+radius);
-        wallLines1[1] = new WallLine(x + radius, y, horizontalWidth, horizontalHeight);
-        wallLines1[2] = new WallLine(x + horizontalWidth+ radius, y+radius, verticalWidth, verticalHeight);
-        wallLines1[3] = new WallLine(x + horizontalWidth+ 2*radius, y +verticalHeight+radius, horizontalWidth, horizontalHeight);
-        wallLines1[4] = new WallLine(x + 2*radius + 2*horizontalWidth, y+radius, verticalWidth, verticalHeight);
-        wallLines1[5] = new WallLine(x + 2*horizontalWidth+3*radius, y, horizontalWidth, horizontalHeight);
+        wallLines1[0] = new WallLine(x, y, verticalWidth, verticalHeight+radius,x,y);
+        wallLines1[1] = new WallLine(x + radius, y, horizontalWidth, horizontalHeight,x + radius, y);
+        wallLines1[2] = new WallLine(x + horizontalWidth+ radius, y+radius, verticalWidth, verticalHeight,x + horizontalWidth+ radius, y+radius);
+        wallLines1[3] = new WallLine(x + horizontalWidth+ 2*radius, y +verticalHeight+radius, horizontalWidth, horizontalHeight,x + horizontalWidth+ 2*radius, y +verticalHeight+radius);
+        wallLines1[4] = new WallLine(x + 2*radius + 2*horizontalWidth, y+radius, verticalWidth, verticalHeight,x + 2*radius + 2*horizontalWidth, y+radius);
+        wallLines1[5] = new WallLine(x + 2*horizontalWidth+3*radius, y, horizontalWidth, horizontalHeight,x + 2*horizontalWidth+3*radius, y);
 
         //create third wall
         y+=120;
         WallEdge wallEdges2[] = new WallEdge[5];
-        wallEdges2[0] = new WallEdge(x+radius+horizontalWidth, y, radius, true);
-        wallEdges2[1] = new WallEdge(x+horizontalWidth+radius, y+radius+verticalHeight,radius,false);
-        wallEdges2[2] = new WallEdge(x+2*radius+2*horizontalWidth, y+radius+verticalHeight,radius,true);
-        wallEdges2[3] = new WallEdge(x, y, radius, false);
-        wallEdges2[4] = new WallEdge(x+2*radius+2*horizontalWidth, y+2*radius+2*verticalHeight, radius, false);
+        wallEdges2[0] = new WallEdge(x+radius+horizontalWidth, y, radius, true,x+radius+horizontalWidth, y);
+        wallEdges2[1] = new WallEdge(x+horizontalWidth+radius, y+radius+verticalHeight,radius,false,x+horizontalWidth+radius, y+radius+verticalHeight);
+        wallEdges2[2] = new WallEdge(x+2*radius+2*horizontalWidth, y+radius+verticalHeight,radius,true,x+2*radius+2*horizontalWidth, y+radius+verticalHeight);
+        wallEdges2[3] = new WallEdge(x, y, radius, false,x, y);
+        wallEdges2[4] = new WallEdge(x+2*radius+2*horizontalWidth, y+2*radius+2*verticalHeight, radius, false,x+2*radius+2*horizontalWidth, y+2*radius+2*verticalHeight);
 
         WallLine wallLines2[] = new WallLine[4];
-        wallLines2[0] = new WallLine(x+radius, y, horizontalWidth+(radius/2), horizontalHeight);
-        wallLines2[1] = new WallLine(x+horizontalWidth+radius, y+radius, verticalWidth, verticalHeight);
-        wallLines2[2] = new WallLine(x+horizontalWidth+2*radius,y+ radius+verticalHeight, horizontalWidth+(radius/2), horizontalHeight);
-        wallLines2[3] = new WallLine(x+2*radius+2*horizontalWidth, y +verticalHeight+2*radius, verticalWidth, verticalHeight);
+        wallLines2[0] = new WallLine(x+radius, y, horizontalWidth+(radius/2), horizontalHeight,x+radius, y);
+        wallLines2[1] = new WallLine(x+horizontalWidth+radius, y+radius, verticalWidth, verticalHeight,x+horizontalWidth+radius, y+radius);
+        wallLines2[2] = new WallLine(x+horizontalWidth+2*radius,y+ radius+verticalHeight, horizontalWidth+(radius/2), horizontalHeight,x+horizontalWidth+2*radius,y+ radius+verticalHeight);
+        wallLines2[3] = new WallLine(x+2*radius+2*horizontalWidth, y +verticalHeight+2*radius, verticalWidth, verticalHeight,x+2*radius+2*horizontalWidth, y +verticalHeight+2*radius);
 
         //create fourth wall
         y+=140;
         WallEdge wallEdges3[] = new WallEdge[6];
-        wallEdges3[0] = new WallEdge(x+horizontalWidth+radius, y,radius,true);
-        wallEdges3[1] = new WallEdge(x, y, radius, false);
-        wallEdges3[2] = new WallEdge(x+horizontalWidth+radius, y+radius+verticalHeight,radius,false);
-        wallEdges3[3] = new WallEdge(x+2*horizontalWidth+2*radius, y+radius+verticalHeight,radius,false);
-        wallEdges3[4] = new WallEdge(x, y+radius+verticalHeight, radius, false);
-        wallEdges3[5] = new WallEdge(x+2*horizontalWidth+2*radius, y+2*radius+2*verticalHeight, radius, false);
+        wallEdges3[0] = new WallEdge(x+horizontalWidth+radius, y,radius,true,x+horizontalWidth+radius, y);
+        wallEdges3[1] = new WallEdge(x, y, radius, false,x, y);
+        wallEdges3[2] = new WallEdge(x+horizontalWidth+radius, y+radius+verticalHeight,radius,false,x+horizontalWidth+radius, y+radius+verticalHeight);
+        wallEdges3[3] = new WallEdge(x+2*horizontalWidth+2*radius, y+radius+verticalHeight,radius,false,x+2*horizontalWidth+2*radius, y+radius+verticalHeight);
+        wallEdges3[4] = new WallEdge(x, y+radius+verticalHeight, radius, false,x, y+radius+verticalHeight);
+        wallEdges3[5] = new WallEdge(x+2*horizontalWidth+2*radius, y+2*radius+2*verticalHeight, radius, false,x+2*horizontalWidth+2*radius, y+2*radius+2*verticalHeight);
 
         WallLine wallLines3[] = new WallLine[5];
-        wallLines3[0] = new WallLine(x, y+radius, verticalWidth, verticalHeight);
-        wallLines3[1] = new WallLine(x+radius, y, horizontalWidth+(radius/2), horizontalHeight);
-        wallLines3[2] = new WallLine(x+horizontalWidth+radius,y+ radius, verticalWidth, verticalHeight);
-        wallLines3[3] = new WallLine(x+horizontalWidth+2*radius, y +verticalHeight+radius, horizontalWidth, horizontalHeight);
-        wallLines3[4] = new WallLine(x+2*horizontalWidth+2*radius, y+radius*2+verticalHeight, verticalWidth, verticalHeight);
+        wallLines3[0] = new WallLine(x, y+radius, verticalWidth, verticalHeight,x, y+radius);
+        wallLines3[1] = new WallLine(x+radius, y, horizontalWidth+(radius/2), horizontalHeight,x+radius, y);
+        wallLines3[2] = new WallLine(x+horizontalWidth+radius,y+ radius, verticalWidth, verticalHeight,x+horizontalWidth+radius,y+ radius);
+        wallLines3[3] = new WallLine(x+horizontalWidth+2*radius, y +verticalHeight+radius, horizontalWidth, horizontalHeight,x+horizontalWidth+2*radius, y +verticalHeight+radius);
+        wallLines3[4] = new WallLine(x+2*horizontalWidth+2*radius, y+radius*2+verticalHeight, verticalWidth, verticalHeight,x+2*horizontalWidth+2*radius, y+radius*2+verticalHeight);
 
 
         walls[0] = new Wall(wallEdges0, wallLines0, 1);
