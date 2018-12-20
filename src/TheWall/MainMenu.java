ac9645336093229c9c5d2a74d9cadd41c3606d36
aeyc <abegumtascioglu@gmail.com>
2018-12-21 02:51:28 +0300

    fonts are changed

--- src/TheWall/MainMenu.java
+++ src/TheWall/MainMenu.java
@@ -13,19 +13,22 @@ Creator: Erdem Ege Marasli and Ayca Begum Tascioglu
  */
 public class MainMenu extends SimplePanel
 {
-
+    public static final Color VERY_LIGHT_BLUE = new Color(51,204,255);
     JButton button1 = new JButton("Play Game");
     JButton button2 = new JButton("Credits");
     JButton button3 = new JButton("Settings");
     JButton button4 = new JButton("How to Play");
+    //51-204-255
     //private final ImageIcon img = new ImageIcon("images/Background.jpg");
     //Image img = Toolkit.getDefaultToolkit().createImage("images/Background.jpg");
     public MainMenu()
     {
         super();
+        JButton buttons[] = {button1,button2,button3,button4};
         type = "playScreen";
         type = "mainScreen";
         JLabel wallString = new JLabel("The Wall");
+        wallString.setForeground(Color.RED);
         wallString.setFont(new Font("Comic Sans MS", Font.PLAIN, 24));
         this.add(wallString);
         this.setLayout(new BoxLayout(this,BoxLayout.Y_AXIS));
@@ -39,6 +42,9 @@ public class MainMenu extends SimplePanel
         button3.addActionListener(buttonListen);
         button4.setFont(new Font("Comic Sans MS", Font.PLAIN, 24));
         button4.addActionListener(buttonListen);
+        for(int i = 0; i < 4; i++){
+            buttons[i].setBackground(VERY_LIGHT_BLUE);
+        }
         this.add(button1);
         this.add(button2);
         this.add(button3);
