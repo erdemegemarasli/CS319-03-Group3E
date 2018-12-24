8cb2f0d9493f51f982c160837841543229192ac9
Erdem <ege.marasli@ug.bilkent.edu.tr>
2018-12-24 18:57:03 +0300

    Update for How to play

--- src/TheWall/HowToPlayScreen.java
+++ src/TheWall/HowToPlayScreen.java
@@ -20,12 +20,18 @@ public class HowToPlayScreen extends SimplePanel {
         ButtonListener buttonListen = new ButtonListener();
         JLabel info = new JLabel("Choose a challenge. ");
         JLabel info1 = new JLabel("Choose a wall with left click of mouse");
-        JLabel info2 = new JLabel( "Rotate the choosen wall with left click of mouse");
-        JLabel info3 = new JLabel(" and place the wall with right click");
-        JLabel info4 = new JLabel( "The red knights needed to kept outside, the blue ");
-        JLabel info41 = new JLabel("knights and high towers needed to remain within the walls.");
+        JLabel info2 = new JLabel( "Rotate the choosen wall with right click of mouse");
+        JLabel info3 = new JLabel(" and place the wall with left click. The red");
+        JLabel info4 = new JLabel( "knights needed to kept outside, the blue knights and ");
+        JLabel info41 = new JLabel("high towers needed to remain within the walls.");
         JLabel info5 = new JLabel("There is only one solution per challenge.");
-
+        info.setForeground(Color.GREEN);
+        info1.setForeground(Color.GREEN);
+        info2.setForeground(Color.GREEN);
+        info3.setForeground(Color.GREEN);
+        info4.setForeground(Color.GREEN);
+        info41.setForeground(Color.GREEN);
+        info5.setForeground(Color.GREEN);
         JLabel texts[] = {info, info1,info2,info3, info4,info41};
         for ( int i = 0; i < texts.length; i++){
             texts[i].setFont(new Font("Comic Sans MS", Font.PLAIN, 15));
