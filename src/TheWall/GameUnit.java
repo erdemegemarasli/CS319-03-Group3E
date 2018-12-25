54b562c57608899d80cb39bb81f414d15addf7d0
aeyc <abegumtascioglu@gmail.com>
2018-12-25 04:15:41 +0300

    javadoc added

--- src/TheWall/GameUnit.java
+++ src/TheWall/GameUnit.java
@@ -14,10 +14,10 @@ public class GameUnit extends GameUnits{
     /**
      * GameUnit constructor to construct a game unit such as knights and castle
      * @param x : int - the x coordinate of a game unit
-     * @param y
-     * @param radius
-     * @param isEnemy
-     * @param theme
+     * @param y : int - the y coordinate of a game unit
+     * @param radius : int the radius of a game unit
+     * @param isEnemy : boolean a flag to understand if the knight is an enemy knight(red knight) or not(blue knight)
+     * @param theme : int an ID to decide if the theme (HarryPotter or GameOfThrones) character will be presented on the game units or not(default)
      */
     public GameUnit(int x, int y,int radius,boolean isEnemy, int theme){
         super(x,y,radius,isEnemy, theme);
