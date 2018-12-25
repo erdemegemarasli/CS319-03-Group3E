351e7dfa157ffd8a9ce9f7a8b3fee152f295e833
aeyc <abegumtascioglu@gmail.com>
2018-12-25 04:13:42 +0300

    javadoc added

--- src/TheWall/GameUnit.java
+++ src/TheWall/GameUnit.java
@@ -2,11 +2,23 @@ package TheWall;
 
 
 import java.awt.*;
-/*
+/**
 Creator: Ayca Begum Tascioglu
  */
 
+/**
+ * Game unit class as an instance of the GameUnits
+ */
 public class GameUnit extends GameUnits{
+
+    /**
+     * GameUnit constructor to construct a game unit such as knights and castle
+     * @param x : int - the x coordinate of a game unit
+     * @param y
+     * @param radius
+     * @param isEnemy
+     * @param theme
+     */
     public GameUnit(int x, int y,int radius,boolean isEnemy, int theme){
         super(x,y,radius,isEnemy, theme);
         //new class knight renamed
